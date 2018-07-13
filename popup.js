eb833792210ad870043cae11be4de262a367efd7
samteezy <sam.g.taylor@gmail.com>
2018-07-13 17:39:23 -0400

    0.0.2
    Updated manifest.
    Removed tabs permission.
    Added icons.

--- popup.js
+++ popup.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var btn = document.getElementById('mybtn');
-  console.log(btn);
-  btn.addEventListener('click', function() {
-	query = document.getElementById("q").value;
-	console.log(query);
-	url ='https://sfdcsearch.com/#gsc.tab=0&gsc.q=' + query;
-	console.log(url);
-	chrome.tabs.create({ url: url });
-  });
+	var input = document.getElementById("q");
+	// Execute a function when the user releases a key on the keyboard
+	input.addEventListener("keyup", function(event) {
+	  // Cancel the default action, if needed
+	  event.preventDefault();
+	  // Number 13 is the "Enter" key on the keyboard
+	  if (event.keyCode === 13) {
+		// Trigger the button element with a click
+		document.getElementById("mybtn").click();
+	  }
+	});
+	var btn = document.getElementById('mybtn');
+		console.log(btn);
+	btn.addEventListener('click', function() {
+		query = document.getElementById("q").value;
+			console.log(query);
+		url ='https://sfdcsearch.com/#gsc.tab=0&gsc.q=' + query;
+			console.log(url);
+		chrome.tabs.create({ url: url });
+	});
 });
